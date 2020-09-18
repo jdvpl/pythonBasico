d728cdd5ff015a74c1acbfa8a53a69f99ff1f0a6
jdvpl <juanda554242@gmail.com>
2020-09-18 12:27:09 -0500

    cambio de variable

--- variable.py
+++ variable.py
@@ -9,3 +9,9 @@ print(texto)
 print(texto2)
 print(num)
 print(num2)
+print("##########################".center(50))
+print("Cambia las variables".center(50))
+num=25
+num2=36
+print(num)
+print(num2)
